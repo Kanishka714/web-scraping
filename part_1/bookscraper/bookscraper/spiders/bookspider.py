91d81d85907c592eec77797926992ad2e0f552f8
Kanishka Rathnayake <kanishkadilshan861@gmail.com>
2024-08-09 14:43:52 +0530

    Save data to MySQL database

--- part_1/bookscraper/bookscraper/spiders/bookspider.py
+++ part_1/bookscraper/bookscraper/spiders/bookspider.py
@@ -6,6 +6,12 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["http://books.toscrape.com/"]
 
+    custom_settings = {
+        'FEEDS' : {
+            'bookdata.json' : {'format': 'json', 'overwrite' : True},
+        }
+    } 
+
     def parse(self, response):
         books = response.css('article.product_pod')
 
@@ -25,6 +31,7 @@ class BookspiderSpider(scrapy.Spider):
 
         book_item['url'] = response.url
         book_item['title'] = response.css('.product_main h1 ::text').get()
+        book_item['upc'] = table_rows[0].css('td ::text').get()
         book_item['product_type'] = table_rows[1].css('td ::text').get()
         book_item['price_excl_tax'] = table_rows[2].css('td ::text').get()
         book_item['price_incl_tax'] = table_rows[3].css('td ::text').get()
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kanishkarathnayake/Desktop/101/part_1/venv"
+set -gx VIRTUAL_ENV "/Users/kanishkarathnayake/Desktop/101/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
